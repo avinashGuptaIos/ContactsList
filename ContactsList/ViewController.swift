72b4f36fd8a988d26744d97ae4f1c80cf86ba993
avinashGuptaIos <avinash.gupta@hashedin.com>
2020-07-25 15:19:10 +0530

    Initial commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by hasher on 25/07/20.
+//  Copyright © 2020 Avinash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
